3650fec29ba6c9aa3dfd2c86a8188ebf5ccd2c1c
Svend Sorensen <svends@uw.edu>
2012-08-21 10:41:05 -0700

    Install firefox

--- puppet/site.pp
+++ puppet/site.pp
@@ -31,4 +31,14 @@ node default {
   class { 'apache': }
   class { 'mysql::server': }
   class { 'tomcat': }
+
+  # Additional packages to install
+
+  $packagelist = [
+    'firefox',
+  ]
+
+  package { $packagelist:
+    ensure => installed,
+  }
 }
