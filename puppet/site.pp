44f6a0cbfaacdc5d11eaa0b5beefd03a6107d86b
Svend Sorensen <svends@uw.edu>
2012-08-23 13:30:59 -0700

    Remove code required by old version of facter
    Facter is installed from the puppetlabs repo, and this version supports the
    osfamily fact.

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,23 +1,3 @@
-if ! $::osfamily {
-  case $::operatingsystem {
-    'RedHat', 'Fedora', 'CentOS', 'Scientific', 'SLC', 'Ascendos', 'CloudLinux', 'PSBM', 'OracleLinux', 'OVS', 'OEL': {
-      $osfamily = 'RedHat'
-    }
-    'ubuntu', 'debian': {
-      $osfamily = 'Debian'
-    }
-    'SLES', 'SLED', 'OpenSuSE', 'SuSE': {
-      $osfamily = 'Suse'
-    }
-    'Solaris', 'Nexenta': {
-      $osfamily = 'Solaris'
-    }
-    default: {
-      $osfamily = $::operatingsystem
-    }
-  }
-}
-
 node default {
   class { 'apt::backports': }
 
