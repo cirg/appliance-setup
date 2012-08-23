3a1a888a247de22906842d7a2129e0615c292e97
Svend Sorensen <svends@uw.edu>
2012-08-23 14:38:52 -0700

    Add default-ssl vhost and ssl-cert package

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,6 +1,21 @@
 node default {
   class { 'apache': }
   class { 'apache::mod::ssl': }
+
+  file { '/etc/apache2/sites-enabled/default-ssl':
+    ensure  => link,
+    target  => '/etc/apache2/sites-available/default-ssl',
+    require => [
+      Package['httpd'],
+      Package['ssl-cert'],
+    ],
+    notify  => Service['httpd'],
+  }
+
+  package { 'ssl-cert':
+    ensure => installed,
+  }
+
   class { 'java': }
   class { 'mysql::server':
     config_hash => { 'root_password' => 'UP9aeKas' }
