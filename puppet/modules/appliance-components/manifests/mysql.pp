81bcf64dfbbbae8e18c327ddd27529752ad1a379
Svend Sorensen <svends@uw.edu>
2012-08-30 12:33:58 -0700

    Move mysql root password to hiera database

--- puppet/modules/appliance-components/manifests/mysql.pp
+++ puppet/modules/appliance-components/manifests/mysql.pp
@@ -1,5 +1,5 @@
 class appliance-components::mysql {
   class { '::mysql::server':
-    config_hash => { 'root_password' => 'UP9aeKas' }
+    config_hash => { 'root_password' => hiera('mysql_root_password') }
   }
 }
