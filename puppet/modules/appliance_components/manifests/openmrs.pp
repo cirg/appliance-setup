4f9a87b146ffb0091acc3a57d1b40e2ae07d9515
Svend Sorensen <svends@uw.edu>
2012-09-04 15:40:08 -0700

    Add component dependencies to openmrs component

--- puppet/modules/appliance_components/manifests/openmrs.pp
+++ puppet/modules/appliance_components/manifests/openmrs.pp
@@ -1,4 +1,9 @@
 class appliance_components::openmrs {
+  include appliance_components::apache
+  include appliance_components::java
+  include appliance_components::mysql
+  include appliance_components::tomcat
+
   file { '/etc/apache2/conf.d/openmrs-proxy.conf':
     ensure  => present,
     source  => 'puppet:///modules/appliance_components/openmrs-proxy.conf',
