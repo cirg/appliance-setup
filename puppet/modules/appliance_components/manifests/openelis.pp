2f7d9e835851f836e672b674b5465d674e2244ae
Svend Sorensen <svends@uw.edu>
2012-09-05 11:09:48 -0700

    Add openelis component

---
+++ puppet/modules/appliance_components/manifests/openelis.pp
@@ -0,0 +1,6 @@
+class appliance_components::openelis {
+  include appliance_components::apache
+  include appliance_components::java
+  include appliance_components::postgresql
+  include appliance_components::tomcat
+}
