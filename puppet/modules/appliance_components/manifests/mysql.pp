fd30751519fcd63671005a882453bca5ae70cc77
Svend Sorensen <svends@uw.edu>
2012-10-29 10:16:26 -0700

    Make /etc/my.cnf a link to /root/.my.cnf
    The database* providers that come with puppetlabs-mysql are failing to
    get the root credentials from /root/.my.cnf. Linking this file to
    /etc/my.cnf fixes this until the problem can be resolved.

--- puppet/modules/appliance_components/manifests/mysql.pp
+++ puppet/modules/appliance_components/manifests/mysql.pp
@@ -6,4 +6,12 @@ class appliance_components::mysql {
   class { '::mysql::server':
     config_hash => { 'root_password' => hiera('mysql_root_password') }
   }
+
+  # The mysql module database* providers fail to use the root
+  # credentials from /root/.my.cnf. Creating /etc/my.cnf fixes this
+  # until the issue can be resolved.
+  file { '/etc/my.cnf':
+    ensure => link,
+    target => '/root/.my.cnf',
+  }
 }
