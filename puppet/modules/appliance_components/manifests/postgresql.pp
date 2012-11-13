826758b4f430696ba7dc9456aceb787176ec8448
Svend Sorensen <svends@uw.edu>
2012-11-13 13:21:19 -0800

    Recreate postgres DBs with UTF-8 encoding

--- puppet/modules/appliance_components/manifests/postgresql.pp
+++ puppet/modules/appliance_components/manifests/postgresql.pp
@@ -9,6 +9,13 @@ class appliance_components::postgresql {
   # Set up locales before installing postgresql
   Class['::locales'] -> Class['::postgresql::server']
 
+  # Recreate databases using UTF-8 locale
+  exec { 'utf8 postgres':
+    command => '/usr/bin/pg_dropcluster --stop 8.4 main ; /usr/bin/pg_createcluster --start --locale en_US.UTF-8 8.4 main',
+    unless  => '/bin/su postgres -c \'psql -t -c \\\l\' | grep template0 | grep -q UTF',
+    require => Package['postgresql-server'],
+  }
+
   # Additional packages to install
 
   $packagelist = [
