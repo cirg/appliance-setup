84cd46e7bf9aa029a472c6524ae2a170f419b1ab
Svend Sorensen <svends@uw.edu>
2012-11-27 14:12:32 -0800

    Set locale before installing postgresql

--- puppet/modules/appliance_components/manifests/postgresql.pp
+++ puppet/modules/appliance_components/manifests/postgresql.pp
@@ -6,6 +6,9 @@ class appliance_components::postgresql {
   include ::locales
   include ::postgresql::server
 
+  # Set up locales before installing postgresql
+  Class['::locales'] -> Class['::postgresql::server']
+
   # Additional packages to install
 
   $packagelist = [
