f1e5bc6f7869e467a4e0fa3cb484e7dd06c07cd8
Svend Sorensen <svends@uw.edu>
2012-11-27 14:59:32 -0800

    Install postgresql-contrib-8.4 package

--- puppet/modules/appliance_components/manifests/postgresql.pp
+++ puppet/modules/appliance_components/manifests/postgresql.pp
@@ -4,4 +4,14 @@
 #
 class appliance_components::postgresql {
   include ::postgresql::server
+
+  # Additional packages to install
+
+  $packagelist = [
+    'postgresql-contrib-8.4',
+  ]
+
+  package { $packagelist:
+    ensure => installed,
+  }
 }
